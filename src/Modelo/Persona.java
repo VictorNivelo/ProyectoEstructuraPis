6e7fe77c6a2a4789dc3154d4885147818f59910d
VictorNivelo <victor.nivelo@unl.edu.ec>
2023-11-23 23:48:11 -0500

    l

--- src/Modelo/Persona.java
+++ src/Modelo/Persona.java
@@ -9,5 +9,67 @@ package Modelo;
  * @author Victor
  */
 public class Persona {
+    private Integer idPersona;
+    private String NumeroCedula;
+    private String Nombre;
+    private String Apellido;
+    private String Telefono;
+
+    public Persona() {
+        
+    }
+
+    public Persona(Integer idPersona, String NumeroCedula, String Nombre, String Apellido, String Telefono) {
+        this.idPersona = idPersona;
+        this.NumeroCedula = NumeroCedula;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Telefono = Telefono;
+    }
+
+    public Integer getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Integer idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNumeroCedula() {
+        return NumeroCedula;
+    }
+
+    public void setNumeroCedula(String NumeroCedula) {
+        this.NumeroCedula = NumeroCedula;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", NumeroCedula=" + NumeroCedula + ", Nombre=" + Nombre + ", Apellido=" + Apellido + ", Telefono=" + Telefono + '}';
+    }
     
 }
