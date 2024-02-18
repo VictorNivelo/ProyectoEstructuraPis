44d54763cf779e7c8263ea566345f2244a7878bd
Victor Nivelo <victor.nivelo@unl.edu.ec>
2024-02-18 05:30:34 -0500

    Nombre docente statico para presentar interfaz vista docente

--- src/Modelo/ControlAccesoDocente.java
+++ src/Modelo/ControlAccesoDocente.java
@@ -8,6 +8,15 @@ package Modelo;
 public class ControlAccesoDocente {
     
     private static Docente docenteLogeado;
+    private static String nombreDocenteLogeado;
+    
+    public static void setNombreDocenteLogeado(String nombre) {
+        nombreDocenteLogeado = nombre;
+    }
+
+    public static String getNombreDocenteLogeado() {
+        return nombreDocenteLogeado;
+    }
 
     public static Docente getDocenteLogeado() {
         return docenteLogeado;
