d28a816e0cea92d4e0275c9a104c27646266801b
Victor Nivelo <104579002+VictorNivelo@users.noreply.github.com>
2024-02-18 00:50:41 -0500

    Eliminacion restos

--- d3/grafo.js
+++
@@ -1,9 +0,0 @@
-var nodes = [{id: 1, label: 'V1'},{id: 2, label: 'V2'},{id: 3, label: 'V3'},];
-var edges = [];
-var container = document.getElementById("mynetwork");
-var data = {
-  nodes: nodes,
-  edges: edges,
-};
-var options = {};
-var network = new vis.Network(container, data, options);
